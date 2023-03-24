5adecddb06188b383372781e53b53250da895200
jinxin-wang <96939056+jinxin-wang@users.noreply.github.com>
2023-03-24 15:06:43 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,5 +4,5 @@
 #### type_seq: [WGS|WES], default: WGS
 #### mode: [TvN|TvNp|Tp|T], default: TvN
 
-/mnt/beegfs/software/snakemake/5.23.0/bin/snakemake -c 'sbatch --cpus-per-task={threads} --mem={resources.mem_mb}M -p {params.queue}' --jobs 20 --latency-wait 50 --rerun-incomplete --config samples=human seq_type=WGS
+/mnt/beegfs/software/snakemake/5.23.0/bin/snakemake -c 'sbatch --cpus-per-task={threads} --mem={resources.mem_mb}M -p {params.queue}' --jobs 20 --latency-wait 50 --rerun-incomplete --config samples=human seq_type=WGS mode=TvN
 
